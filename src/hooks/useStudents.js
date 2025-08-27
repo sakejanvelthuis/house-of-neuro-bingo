9a991e328cb2f5bbaf095aeb70d6c0150733502c
Sake Jan <sakejanvelthuis@gmail.com>
2025-08-27 22:35:01 +0200

    fix: restore student login by using original storage key

--- src/hooks/useStudents.js
+++ src/hooks/useStudents.js
@@ -1,6 +1,7 @@
 import usePersistentState from './usePersistentState';
 import seedStudents from '../data/students.json';
 
+// bump this key when the students.json seed data changes
 const LS_KEY = 'nm_points_students_v2';
 
 export default function useStudents() {
